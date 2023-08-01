9c0948f07f9e689776d15c97fbe0683b5270f1f3
Stefan Feiler <65195093+youngfeiler@users.noreply.github.com>
2023-08-01 01:07:52 +0000

    suck my sack

--- functionality/live_data_collector.py
+++ functionality/live_data_collector.py
@@ -117,6 +117,7 @@ class data_collector():
         # Replace some bad values same as we did in the creation of the model
         self.replace_bad_vals_for_split()
         #print(f'Length after replace bad vals: {len(self.df)}')
+        # suck my sack
         
         #  DON'T TOUCH. ORDER IS VERY IMPORTANT
         column_order = ['barstool_1_odds', 'betclic_1_odds',  
